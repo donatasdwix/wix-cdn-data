d83ed09ef7b7291127832356f44996f92b42c0ec
Ofir Dagan <ofird@wix.com>
2014-03-05 12:13:17 +0200

    added angular 1.2.14

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function wixCdnData(host) {
 
   angularFiles.forEach(function (item) {
     data[item] = {
-      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10'],
+      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10','1.2.14'],
       recursive: (item === 'angular-i18n'),
       url: function (version) {
         if (item === 'angular-i18n') {
