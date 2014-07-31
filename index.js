e5ebe75f8a83f16ee217c971241574f7c18f9477
Shahar Talmi <shahar.talmi@gmail.com>
2014-07-31 22:21:11 +0300

    adding vm

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
 'use strict';
 
-function wixCdnData(host) {
+function wixCdnData(host, fn) {
+  fn = fn || function (str) { return str; };
   var data = {
     jquery: {
       versions: ['1.8.3', '1.9.0', '2.0.3', '2.1.0', '2.1.1'],
       url: function (version) {
-        return host + '/services/third-party/jquery/' + version + '/jquery.min.js';
+        return fn(host + 'services/third-party/jquery/' + version + '/jquery.min.js');
       }
     },
     'jquery-ui': {
       versions: ['1.8.24'],
       url: function (version) {
-        return host + '/services/third-party/jqueryui/' + version + '/jquery-ui.min.js';
+        return fn(host + 'services/third-party/jqueryui/' + version + '/jquery-ui.min.js');
       }
     },
     'es5-shim': {
       versions: ['2.1.0', '2.3.0', '3.0.2', '3.4.0', '4.0.1'],
       url: function (version) {
-        return host + '/services/third-party/es5-shim/' + version + '/es5-shim.min.js';
+        return fn(host + 'services/third-party/es5-shim/' + version + '/es5-shim.min.js');
       }
     },
     'angular-translate': {
       versions: ['1.1.1', '1.1.0', '2.0.1', '2.1.0', '2.2.0'],
       url: function (version) {
-        return host + '/services/third-party/angular-translate/' + version + '/angular-translate.min.js';
+        return fn(host + 'services/third-party/angular-translate/' + version + '/angular-translate.min.js');
       }
     },
     'angular-bootstrap': {
       versions: ['0.10.0', '0.9.0', '0.11.0'],
       url: function (version) {
-        return host + '/services/third-party/angular-bootstrap/' + version + '/ui-bootstrap-tpls.min.js';
+        return fn(host + 'services/third-party/angular-bootstrap/' + version + '/ui-bootstrap-tpls.min.js');
       }
     },
     underscore: {
       versions: ['1.5.2', '1.6.0'],
       url: function (version) {
-        return host + '/services/third-party/underscore/' + version + '/underscore-min.js';
+        return fn(host + 'services/third-party/underscore/' + version + '/underscore-min.js');
       }
     },
     lodash: {
       versions: ['2.4.1'],
       url: function (version) {
-        return host + '/services/third-party/lodash/' + version + '/dist/lodash.min.js';
+        return fn(host + 'services/third-party/lodash/' + version + '/dist/lodash.min.js');
+      }
+    },
+    requirejs: {
+      versions: ['2.1.14'],
+      url: function (version) {
+        return fn(host + 'services/third-party/requirejs/' + version + '/require.min.js');
       }
     },
     swfobject: {
       versions: ['2.2'],
       url: function (version) {
-        return host + '/services/third-party/swfobject/' + version + '/swfobject.js';
+        return fn(host + 'services/third-party/swfobject/' + version + '/swfobject.js');
       }
     }
   };
@@ -66,13 +73,13 @@ function wixCdnData(host) {
 
   angularFiles.forEach(function (item) {
     data[item] = {
-      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10', '1.2.14', '1.2.16', '1.2.18', '1.2.19', '1.2.20'],
+      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10', '1.2.14', '1.2.16', '1.2.18', '1.2.19', '1.2.20', '1.2.21'],
       recursive: (item === 'angular-i18n'),
       url: function (version) {
         if (item === 'angular-i18n') {
-          return host + '/services/third-party/angularjs/' + version + '/i18n';
+          return fn(host + 'services/third-party/angularjs/' + version + '/i18n');
         } else {
-          return host + '/services/third-party/angularjs/' + version + '/' + item + '.min.js';
+          return fn(host + 'services/third-party/angularjs/' + version + '/' + item + '.min.js');
         }
       }
     };
@@ -82,6 +89,9 @@ function wixCdnData(host) {
 }
 
 module.exports = {
-  http: wixCdnData.bind(undefined, 'http://static.parastorage.com'),
-  https: wixCdnData.bind(undefined, 'https://sslstatic.wix.com')
+  http: wixCdnData.bind(undefined, 'http://static.parastorage.com/'),
+  https: wixCdnData.bind(undefined, 'https://sslstatic.wix.com/'),
+  vm: wixCdnData.bind(undefined, '${staticBaseUrl}', function (str) {
+    return str.replace(/\.min\.js$/, '#if(!${debug}).min#{end}.js');
+  })
 };
