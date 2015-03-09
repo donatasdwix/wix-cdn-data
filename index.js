3a4ca86c4bb4603814448d1a8c85b8a43aecc9cb
Shahar Talmi <shahar.talmi@gmail.com>
2015-03-09 10:02:04 +0200

    update libs

--- index.js
+++ index.js
@@ -26,43 +26,43 @@ function wixCdnData(host, fn) {
       }
     },
     'jquery-ui': {
-      versions: ['1.8.24', '1.11.2'],
+      versions: ['1.8.24', '1.11.2', '1.11.3'],
       url: function (version) {
         return fn(host + 'services/third-party/jqueryui/' + version + '/jquery-ui.min.js');
       }
     },
     'es5-shim': {
-      versions: ['2.1.0', '2.3.0', '3.0.2', '3.4.0', '4.0.1', '4.0.3', '4.0.5', '4.0.6'],
+      versions: ['2.1.0', '2.3.0', '3.0.2', '3.4.0', '4.0.1', '4.0.3', '4.0.5', '4.0.6', '4.1.0'],
       url: function (version) {
         return fn(host + 'services/third-party/es5-shim/' + version + '/es5-shim.min.js');
       }
     },
     'angular-translate': {
-      versions: ['1.1.1', '1.1.0', '2.0.1', '2.1.0', '2.2.0', '2.3.0', '2.4.1', '2.4.2', '2.5.2'],
+      versions: ['1.1.1', '1.1.0', '2.0.1', '2.1.0', '2.2.0', '2.3.0', '2.4.1', '2.4.2', '2.5.2', '2.6.1'],
       url: function (version) {
         return fn(host + 'services/third-party/angular-translate/' + version + '/angular-translate.min.js');
       }
     },
     'angular-bootstrap': {
-      versions: ['0.10.0', '0.9.0', '0.11.0', '0.11.2', '0.12.0'],
+      versions: ['0.10.0', '0.9.0', '0.11.0', '0.11.2', '0.12.0', '0.12.1'],
       url: function (version) {
         return fn(host + 'services/third-party/angular-bootstrap/' + version + '/ui-bootstrap-tpls.min.js');
       }
     },
     underscore: {
-      versions: ['1.5.2', '1.6.0', '1.7.0'],
+      versions: ['1.5.2', '1.6.0', '1.7.0', '1.8.2'],
       url: function (version) {
         return fn(host + 'services/third-party/underscore/' + version + '/underscore-min.js');
       }
     },
     lodash: {
-      versions: ['2.4.1', '3.0.0', '3.1.0'],
+      versions: ['2.4.1', '3.0.0', '3.1.0', '3.5.0'],
       url: function (version) {
         return fn(host + 'services/third-party/lodash/' + version + '/dist/lodash.min.js');
       }
     },
     requirejs: {
-      versions: ['2.1.14', '2.1.15'],
+      versions: ['2.1.14', '2.1.15', '2.1.16'],
       url: function (version) {
         return fn(host + 'services/third-party/requirejs/' + version + '/require.min.js');
       }
@@ -96,7 +96,7 @@ function wixCdnData(host, fn) {
 
   angularFiles.forEach(function (item) {
     data[item] = {
-      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10', '1.2.14', '1.2.16', '1.2.18', '1.2.19', '1.2.20', '1.2.21', '1.2.25', '1.2.27', '1.2.28', '1.3.0', '1.3.3', '1.3.4', '1.3.8', '1.3.11', '1.3.12'],
+      versions: ['1.0.8', '1.1.4', '1.1.5', '1.2.1', '1.2.6', '1.2.10', '1.2.14', '1.2.16', '1.2.18', '1.2.19', '1.2.20', '1.2.21', '1.2.25', '1.2.27', '1.2.28', '1.3.0', '1.3.3', '1.3.4', '1.3.8', '1.3.11', '1.3.12', '1.3.14'],
       recursive: (item === 'angular-i18n'),
       url: function (version) {
         if (item === 'angular-i18n') {
